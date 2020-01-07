d6f966c45e7c78ca1fa05f109b275ce133f2afc3
mike.zhou <694335199@qq.com>
2020-01-07 20:57:50 +0800

    增加仪表板和身份验证

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {PostList, PostEdit, PostCreate} from './posts'
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
